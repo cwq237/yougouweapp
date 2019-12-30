249b9cfd5caafa3db961806555035e354c666e32
陈皮糖 <510939237@qq.com>
2019-12-30 23:45:21 +0800

    分类，楼层一半

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,30 @@
-// Page Object
+// 引入用来发送请求的方法
+import { request } from "../../request/index.js";
+
 Page({
   data:{
+    // 轮播图数组
+    swiperList:[],
+    // 分类数据
+    cateList:[],
+    // 楼层数据
+    floorList:[],
 
   },
   onLoad:function(options){
+    // 1. 发送异步请求获取检查轮播图数据
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success:(result)=>{
+    //     console.log(result)
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   },
+    // })
+
+    this.getSwiperList()
+    this.getCateList()
 
   },
   onReady:function(){
@@ -17,5 +38,32 @@ Page({
   },
   onUnload:function(){
     
+  },
+
+  // 获取轮播图数据
+  getSwiperList(){
+    request({url:'https://api.zbztb.cn/api/public/v1/home/swiperdata'}).then(result=>{
+      this.setData({
+        swiperList: result.data.message
+      })
+    })
+  },
+  // 分类数据
+  getCateList(){
+    request({url:'https://api.zbztb.cn/api/public/v1/home/catitems'}).then(result=>{
+      console.log(result)
+      this.setData({
+        cateList: result.data.message
+      })
+    })
+  },
+  // 楼层数据
+  getFoorList(){
+    request({url:'https://api.zbztb.cn/api/public/v1/home/floordata'}).then(result=>{
+      console.log(result)
+      this.setData({
+        floorList: result.data.message
+      })
+    })
   }
 })
